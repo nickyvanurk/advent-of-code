4ba30082924438619d985b0e5c79caa029d1072f
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-01 22:43:06 +0100

    Simplify day01 solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,44 +1,39 @@
 pub fn part1(input: String) {
-    let expense_report: Vec<i32> = input
+    let report: Vec<i32> = input
         .trim()
         .split('\n')
         .map(|s: &str| s.parse::<i32>().unwrap())
         .collect();
 
-    println!(
-        "{}",
-        find_operands_of_sum(2020, &expense_report)
-            .iter()
-            .product::<i32>()
-    );
+    let n = report.len();
+
+    for i in 0..n {
+        for j in i + 1..n {
+            if report[i] + report[j] == 2020 {
+                println!("{}", report[i] * report[j]);
+                break;
+            }
+        }
+    }
 }
 
 pub fn part2(input: String) {
-    let expense_report: Vec<i32> = input
+    let report: Vec<i32> = input
         .trim()
         .split('\n')
         .map(|s: &str| s.parse::<i32>().unwrap())
         .collect();
 
-    for expense in &expense_report {
-        let mut operands = find_operands_of_sum(2020 - expense, &expense_report);
+    let n = report.len();
 
-        if operands.len() == 2 {
-            operands.append(&mut vec![expense.clone()]);
-            println!("{}", operands.iter().product::<i32>());
-            break;
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                if report[i] + report[j] + report[k] == 2020 {
+                    println!("{}", report[i] * report[j] * report[k]);
+                    break;
+                }
+            }
         }
     }
-}
-
-fn find_operands_of_sum(sum: i32, data: &Vec<i32>) -> Vec<i32> {
-    for operand_a in data {
-        let operand_b = sum - operand_a;
-
-        if data.contains(&operand_b) {
-            return vec![operand_a.clone(), operand_b];
-        }
-    }
-
-    return vec![];
 }
