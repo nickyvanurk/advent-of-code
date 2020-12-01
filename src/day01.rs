3c748a623f9286ce771f0f26c759eb73de9ad85f
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-01 21:02:21 +0100

    Add day01 part 1 solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,16 @@
-pub fn part1(inp: String) {
-    println!("{}", inp.chars().rev().collect::<String>());
+pub fn part1(input: String) {
+    let expense_report: Vec<i32> = input
+        .trim()
+        .split('\n')
+        .map(|s: &str| s.parse::<i32>().unwrap())
+        .collect();
+
+    for expense in &expense_report {
+        let operand = 2020 - expense;
+
+        if expense_report.contains(&operand) {
+            println!("{} * {} = {}", expense, operand, expense * operand);
+            break;
+        }
+    }
 }
