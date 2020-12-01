7fb0788e6fa51dcafc8bdc7a4d6c52ea5585d537
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-01 22:21:37 +0100

    Add day01 part 2 solution

--- src/day01.rs
+++ src/day01.rs
@@ -5,12 +5,40 @@ pub fn part1(input: String) {
         .map(|s: &str| s.parse::<i32>().unwrap())
         .collect();
 
-    for expense in &expense_report {
-        let operand = 2020 - expense;
+    println!(
+        "{}",
+        find_operands_of_sum(2020, &expense_report)
+            .iter()
+            .product::<i32>()
+    );
+}
 
-        if expense_report.contains(&operand) {
-            println!("{} * {} = {}", expense, operand, expense * operand);
+pub fn part2(input: String) {
+    let expense_report: Vec<i32> = input
+        .trim()
+        .split('\n')
+        .map(|s: &str| s.parse::<i32>().unwrap())
+        .collect();
+
+    for expense in &expense_report {
+        let mut operands = find_operands_of_sum(2020 - expense, &expense_report);
+
+        if operands.len() == 2 {
+            operands.append(&mut vec![expense.clone()]);
+            println!("{}", operands.iter().product::<i32>());
             break;
         }
     }
+}
+
+fn find_operands_of_sum(sum: i32, data: &Vec<i32>) -> Vec<i32> {
+    for operand_a in data {
+        let operand_b = sum - operand_a;
+
+        if data.contains(&operand_b) {
+            return vec![operand_a.clone(), operand_b];
+        }
+    }
+
+    return vec![];
 }
