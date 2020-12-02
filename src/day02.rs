081db688abd31b8b287bcb4d7ea42670f46e0eb4
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-02 15:22:13 +0100

    Simplify day02 solution

--- src/day02.rs
+++ src/day02.rs
@@ -1,71 +1,34 @@
 pub fn part1(input: String) {
-    let passwords: Vec<Vec<&str>> = input
-        .trim()
-        .split('\n')
-        .map(|s: &str| s.split(':').collect())
-        .collect();
+    let valid_passwords = input
+        .lines()
+        .map(|s| {
+            let v: Vec<&str> = s.split(|c| vec!['-', ':', ' '].contains(&c)).collect();
+            let min = v[0].parse::<u8>().unwrap();
+            let max = v[1].parse::<u8>().unwrap();
+            let letter_occurrences = v[4].matches(v[2]).count() as u8;
 
-    let mut valid_passwords = 0;
-
-    for entry in passwords {
-        let policy = entry[0];
-        let password = entry[1];
-
-        let (min, max, letter) = match &policy.split('-').collect::<Vec<&str>>()[..] {
-            &[min, max] => (
-                min.parse::<i32>().unwrap(),
-                max.split(' ').collect::<Vec<&str>>()[0]
-                    .parse::<i32>()
-                    .unwrap(),
-                max.split(' ').collect::<Vec<&str>>()[1],
-            ),
-            _ => unreachable!(),
-        };
-
-        let letter_count = password.matches(letter).count() as i32;
-
-        if min <= letter_count && letter_count <= max {
-            valid_passwords += 1;
-        }
-    }
+            min <= letter_occurrences && letter_occurrences <= max
+        })
+        .filter(|b| *b)
+        .count();
 
     println!("{}", valid_passwords);
 }
 
 pub fn part2(input: String) {
-    let passwords: Vec<Vec<&str>> = input
-        .trim()
-        .split('\n')
-        .map(|s: &str| s.split(':').collect())
-        .collect();
+    let valid_passwords = input
+        .lines()
+        .map(|s| {
+            let v: Vec<&str> = s.split(|c| vec!['-', ':', ' '].contains(&c)).collect();
+            let pos1 = v[0].parse::<usize>().unwrap();
+            let pos2 = v[1].parse::<usize>().unwrap();
+            let pos1_has_letter = v[4][pos1 - 1..pos1] == *v[2];
+            let pos2_has_letter = v[4][pos2 - 1..pos2] == *v[2];
 
-    let mut valid_passwords = 0;
-
-    for entry in passwords {
-        let policy = entry[0];
-        let password = entry[1];
-
-        let (pos1, pos2, letter) = match &policy.split('-').collect::<Vec<&str>>()[..] {
-            &[pos1, pos2] => (
-                pos1.parse::<i32>().unwrap(),
-                pos2.split(' ').collect::<Vec<&str>>()[0]
-                    .parse::<i32>()
-                    .unwrap(),
-                pos2.split(' ').collect::<Vec<&str>>()[1]
-                    .chars()
-                    .nth(0)
-                    .unwrap(),
-            ),
-            _ => unreachable!(),
-        };
-
-        let pos1_has_letter = password.chars().nth(pos1 as usize).unwrap() == letter;
-        let pos2_has_letter = password.chars().nth(pos2 as usize).unwrap() == letter;
-
-        if (pos1_has_letter != pos2_has_letter) && (pos1_has_letter || pos2_has_letter) {
-            valid_passwords += 1;
-        }
-    }
+            pos1_has_letter as u8 + pos2_has_letter as u8 == 1
+        })
+        .filter(|b| *b)
+        .count();
 
     println!("{}", valid_passwords);
 }
