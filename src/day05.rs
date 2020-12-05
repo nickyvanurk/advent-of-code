75c7c365e439b2fca3c3f8818849db769d2bc67e
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-05 13:51:37 +0100

    Simplify day05 part 1

--- src/day05.rs
+++ src/day05.rs
@@ -1,50 +1,26 @@
 pub fn part1(input: String) {
-    let boarding_passes: Vec<&str> = input.lines().collect();
+    let highest_seat_id = input
+        .lines()
+        .map(|pass| {
+            let mut row_range = (0, 127);
+            let mut col_range = (0, 7);
 
-    let rows = 128;
-    let columns = 8;
-
-    let mut highest_seat_id = 0;
-    let mut row = 0;
-    let mut column = 0;
-
-    for pass in boarding_passes {
-        let mut row_range = (0, rows - 1);
-
-        for c in pass.chars().take(7) {
-            let range = row_range.1 - row_range.0;
-
-            match c {
-                'F' => row_range.1 = row_range.0 + range / 2,
-                'B' => row_range.0 = row_range.1 - range / 2,
-                _ => unreachable!(),
+            for c in pass.chars() {
+                match c {
+                    'F' => row_range.1 = row_range.0 + (row_range.1 - row_range.0) / 2,
+                    'B' => row_range.0 = row_range.1 - (row_range.1 - row_range.0) / 2,
+                    'L' => col_range.1 = col_range.0 + (col_range.1 - col_range.0) / 2,
+                    'R' => col_range.0 = col_range.1 - (col_range.1 - col_range.0) / 2,
+                    _ => unreachable!(),
+                }
             }
 
-            row = row_range.0;
-        }
+            row_range.0 * 8 + col_range.0
+        })
+        .max()
+        .unwrap();
 
-        let mut col_range = (0, columns - 1);
-
-        for c in pass.chars().skip(7) {
-            let range = col_range.1 - col_range.0;
-
-            match c {
-                'L' => col_range.1 = col_range.0 + range / 2,
-                'R' => col_range.0 = col_range.1 - range / 2,
-                _ => unreachable!(),
-            }
-
-            column = col_range.0;
-        }
-
-        let seat_id = row * 8 + column;
-
-        if seat_id > highest_seat_id {
-            highest_seat_id = seat_id;
-        }
-    }
-
-    println!("{}", highest_seat_id);
+    println!("{:?}", highest_seat_id);
 }
 
 pub fn part2(input: String) {
