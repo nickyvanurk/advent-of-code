1d2793435daf33d8c181ec98e6fefa56d1b2c3ec
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-06 22:36:42 +0100

    Refactor day 4

--- 2020/src/day04.rs
+++ 2020/src/day04.rs
@@ -1,78 +1,31 @@
 use regex::Regex;
 
 pub fn part1(input: String) {
-    let input: Vec<&str> = input.lines().collect();
-    let mut passports: Vec<Vec<&str>> = vec![vec![]];
+    let required_fields = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-    for (i, line) in input.iter().enumerate() {
-        if !line.is_empty() {
-            passports.last_mut().unwrap().extend(
-                line.split(|l| vec![':', ' '].contains(&l))
-                    .collect::<Vec<&str>>(),
-            );
-        }
+    let valid_passports = input
+        .split("\n\n")
+        .filter(|&p| required_fields.iter().all(|field| p.contains(field)))
+        .count();
 
-        let next = match input.get(i + 1) {
-            Some(value) => value,
-            None => continue,
-        };
-
-        if next.is_empty() {
-            passports.push(vec![]);
-        }
-    }
-
-    println!(
-        "{}",
-        passports
-            .iter()
-            .filter(|p| {
-                p.contains(&"byr")
-                    && p.contains(&"iyr")
-                    && p.contains(&"eyr")
-                    && p.contains(&"hgt")
-                    && p.contains(&"hcl")
-                    && p.contains(&"ecl")
-                    && p.contains(&"pid")
-            })
-            .count()
-    );
+    println!("{}", valid_passports);
 }
 
 pub fn part2(input: String) {
-    println!(
-        "{}",
-        input
-            .trim()
-            .split("\n\n")
-            .filter(|&p| {
-                return if !Regex::new(r"byr:(19[2-9][0-9]|200[0-2])")
-                    .unwrap()
-                    .is_match(p)
-                {
-                    false
-                } else if !Regex::new(r"iyr:(201[0-9]|2020)").unwrap().is_match(p) {
-                    false
-                } else if !Regex::new(r"eyr:(202[0-9]|2030)").unwrap().is_match(p) {
-                    false
-                } else if !Regex::new(r"hgt:(((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in))")
-                    .unwrap()
-                    .is_match(p)
-                {
-                    false
-                } else if !Regex::new(r"hcl:#[0-9a-f]{6}").unwrap().is_match(p) {
-                    false
-                } else if !Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)")
-                    .unwrap()
-                    .is_match(p)
-                {
-                    false
-                } else if !Regex::new(r"pid:\d{9}(\s|[a-z]|$)").unwrap().is_match(p) {
-                    false
-                } else {
-                    true
-                };
-            })
-            .count()
-    );
+    let validation_rules = vec![
+        Regex::new(r"byr:(19[2-9][0-9]|200[0-2])").unwrap(),
+        Regex::new(r"iyr:(201[0-9]|2020)").unwrap(),
+        Regex::new(r"eyr:(202[0-9]|2030)").unwrap(),
+        Regex::new(r"hgt:(((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in))").unwrap(),
+        Regex::new(r"hcl:#[0-9a-f]{6}").unwrap(),
+        Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)").unwrap(),
+        Regex::new(r"pid:\d{9}(\s|[a-z]|$)").unwrap(),
+    ];
+
+    let valid_passports = input
+        .split("\n\n")
+        .filter(|&passport| validation_rules.iter().all(|re| re.is_match(passport)))
+        .count();
+
+    println!("{}", valid_passports);
 }
