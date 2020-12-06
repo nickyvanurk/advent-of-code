8823ac9897db302fca089ba53b84ee3b1564ef3e
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-06 16:19:40 +0100

    Add 2015/day01

--- 2015/src/day01.rs
+++ 2015/src/day01.rs
@@ -1,3 +1,21 @@
-pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+pub fn part1(instructions: String) {
+    let floor = instructions.matches("(").count() - instructions.matches(")").count();
+
+    println!("{}", floor);
+}
+
+pub fn part2(instructions: String) {
+    let mut floor = 0;
+    let mut position = 0;
+
+    for (i, direction) in instructions.chars().enumerate() {
+        floor += if direction == '(' { 1 } else { -1 };
+
+        if floor == -1 {
+            position = i + 1;
+            break;
+        }
+    }
+
+    println!("{}", position);
 }
--- 2015/src/lib.rs
+++ 2015/src/lib.rs
@@ -7,7 +7,7 @@ pub type DayFn = fn(String);
 
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
-        1 => (day01::part1, noop),
+        1 => (day01::part1, day01::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
