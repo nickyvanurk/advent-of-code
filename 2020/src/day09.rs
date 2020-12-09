a42f7cfc42759fd1cd4a069bec133da468bd4356
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-09 10:09:02 +0100

    Add day 9

---
+++ 2020/src/day09.rs
@@ -0,0 +1,70 @@
+use std::collections::HashSet;
+
+pub fn part1(input: String) {
+    let numbers = input
+        .lines()
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+    let preamble = 25;
+    let mut sum_idx = 25;
+    let mut set: HashSet<i64> = HashSet::new();
+
+    let mut sum;
+    let mut current;
+
+    for i in 0..numbers.len() {
+        sum = numbers[sum_idx];
+        current = numbers[i];
+
+        if i == sum_idx {
+            set.remove(&numbers[((sum_idx as i64) - preamble) as usize]);
+            set.insert(sum);
+            sum_idx += 1;
+        }
+
+        if set.len() == preamble as usize {
+            let mut found = false;
+
+            for j in (i - preamble as usize)..i {
+                if set.contains(&(sum - numbers[j])) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if !found {
+                println!("{}", sum);
+                break;
+            }
+        }
+
+        set.insert(current);
+    }
+}
+
+pub fn part2(input: String) {
+    let numbers = input
+        .lines()
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+    let mut set: HashSet<i64> = HashSet::new();
+    let mut sum: i64;
+    let mut min_idx = 0;
+    let target = 400480901;
+
+    for i in 0..numbers.len() {
+        set.insert(numbers[i]);
+
+        while set.iter().sum::<i64>() > target {
+            set.remove(&numbers[min_idx]);
+            min_idx += 1;
+        }
+
+        sum = set.iter().sum();
+
+        if sum == target {
+            println!("{}", set.iter().min().unwrap() + set.iter().max().unwrap());
+            break;
+        }
+    }
+}
--- 2020/src/lib.rs
+++ 2020/src/lib.rs
@@ -7,6 +7,7 @@ pub mod day05;
 pub mod day06;
 pub mod day07;
 pub mod day08;
+pub mod day09;
 
 pub fn noop(_inp: String) {}
 
@@ -22,6 +23,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         6 => (day06::part1, day06::part2),
         7 => (day07::part1, day07::part2),
         8 => (day08::part1, day08::part2),
+        9 => (day09::part1, day09::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
