76e44d2dba2a3b5be3889a4bf6780e40f986d232
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-13 22:46:26 +0100

    Add day 13 part 2

--- 2020/src/day13.rs
+++ 2020/src/day13.rs
@@ -23,6 +23,34 @@ pub fn part1(input: &String) -> u32 {
     shortest_time_before_departure.0 * shortest_time_before_departure.1
 }
 
-pub fn part2(input: &String) -> u32 {
+pub fn part2(input: &String) -> u64 {
+    let bus_ids = input
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split(',')
+        .enumerate()
+        .filter(|&(_, bus_id)| bus_id != "x")
+        .map(|(i, bus_id)| (i, bus_id.parse::<u64>().unwrap()))
+        .collect::<Vec<(usize, u64)>>();
+
+    let mut time = 0;
+    let product = bus_ids.iter().fold(1, |acc, &(_, bus_id)| acc * bus_id);
+
+    for &(idx, bus_id) in bus_ids.iter() {
+        let pp = product / bus_id;
+        time += (bus_id as i64 - idx as i64) * mod_inverse(pp, bus_id) as i64 * pp as i64;
+    }
+
+    time as u64 % product
+}
+
+fn mod_inverse(a: u64, m: u64) -> u64 {
+    for x in 1..m {
+        if (a * x) % m == 1 {
+            return x;
+        }
+    }
+
     0
 }
