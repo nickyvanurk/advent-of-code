3389d4a565ea4e9d0c4e16b7567b4ca0d8ca014b
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-16 02:04:48 +0100

    Add day 15 part 2

--- 2020/src/day15.rs
+++ 2020/src/day15.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn part1(input: &String) -> u32 {
     let mut spoken_numbers = input
         .trim()
@@ -36,5 +38,29 @@ pub fn part1(input: &String) -> u32 {
 }
 
 pub fn part2(input: &String) -> u32 {
-    0
+    let mut spoken_numbers = input
+        .trim()
+        .split(',')
+        .enumerate()
+        .map(|(i, n)| (n.parse::<u32>().unwrap(), i as u32 + 1))
+        .collect::<HashMap<u32, u32>>();
+
+    let mut last = spoken_numbers.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap();
+    let mut last_spoken_number: (u32, u32) = (*last.0, *last.1);
+    spoken_numbers.remove(&last_spoken_number.0);
+
+    for turn in spoken_numbers.len() + 2..=30000000 {
+        if !spoken_numbers.contains_key(&last_spoken_number.0) {
+            spoken_numbers.insert(last_spoken_number.0, last_spoken_number.1);
+            last_spoken_number = (0, turn as u32);
+        } else {
+            last = spoken_numbers.get_key_value(&last_spoken_number.0).unwrap();
+
+            let l = last_spoken_number;
+            last_spoken_number = ((turn as u32 - 1) - last.1, turn as u32);
+            *spoken_numbers.get_mut(&l.0).unwrap() = l.1;
+        }
+    }
+
+    last_spoken_number.0
 }
