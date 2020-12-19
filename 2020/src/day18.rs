73e9c609edb73a17dc58c4f5faa397f7768afcb7
Nicky van Urk <nickyvanurk@gmx.com>
2020-12-19 23:59:27 +0100

    Add day 18 part 2

--- 2020/src/day18.rs
+++ 2020/src/day18.rs
@@ -3,17 +3,24 @@ pub fn part1(input: &String) -> u64 {
     let mut sum = 0;
 
     while let Some(line) = homework.next() {
-        sum += calculate_homework(&line);
+        sum += calculate_homework(&line, false);
     }
 
     sum
 }
 
-pub fn part2(input: &String) -> u32 {
-    0
+pub fn part2(input: &String) -> u64 {
+    let mut homework = input.lines();
+    let mut sum = 0;
+
+    while let Some(line) = homework.next() {
+        sum += calculate_homework(&line, true);
+    }
+
+    sum
 }
 
-fn calculate_homework(line: &str) -> u64 {
+fn calculate_homework(line: &str, addition_first: bool) -> u64 {
     let chars = line.replace(" ", "").chars().collect::<Vec<char>>();
     let mut homework = Vec::<String>::new();
     let mut open_idxs = Vec::<usize>::new();
@@ -27,7 +34,11 @@ fn calculate_homework(line: &str) -> u64 {
             ')' => {
                 let idx = open_idxs.pop().unwrap();
                 let slice = &homework[idx + 1..i];
-                let result = calculate_expression(slice).to_string();
+                let result = if addition_first {
+                    calculate_expression_addition_first(slice).to_string()
+                } else {
+                    calculate_expression(slice).to_string()
+                };
 
                 for j in 0..slice.len() + 2 {
                     homework.pop();
@@ -45,7 +56,11 @@ fn calculate_homework(line: &str) -> u64 {
         i += 1;
     }
 
-    calculate_expression(&homework)
+    if addition_first {
+        calculate_expression_addition_first(&homework)
+    } else {
+        calculate_expression(&homework)
+    }
 }
 
 fn calculate_expression(chars: &[String]) -> u64 {
@@ -62,4 +77,20 @@ fn calculate_expression(chars: &[String]) -> u64 {
     }
 
     answer
+}
+
+fn calculate_expression_addition_first(chars: &[String]) -> u64 {
+    let mut numbers = Vec::<u64>::new();
+
+    for (i, c) in chars.iter().enumerate() {
+        if c.parse::<u64>().is_ok() && numbers.is_empty() {
+            numbers.push(c.parse::<u64>().unwrap());
+        } else if c == "+" && chars[i + 1].parse::<u64>().is_ok() {
+            *numbers.last_mut().unwrap() += chars[i + 1].parse::<u64>().unwrap();
+        } else if c == "*" && chars[i + 1].parse::<u64>().is_ok() {
+            numbers.push(chars[i + 1].parse::<u64>().unwrap());
+        }
+    }
+
+    numbers.iter().fold(1, |acc, n| acc * n)
 }
