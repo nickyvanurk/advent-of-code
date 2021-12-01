9a127eeccccff47e9969d944e118754c48f0c8d5
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-01 12:19:01 +0100

    Improve speed by 40%

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,9 +1,11 @@
+use itertools::Itertools;
+
 pub fn part1(input: &String) -> u16 {
-    let report = input.lines().map(|s: &str| s.parse::<u16>().unwrap());
-    report.clone().zip(report.skip(1)).filter(|(a, b)| a < b).count() as u16
+    let report = input.lines().map(|s| s.parse::<u16>().unwrap());
+    report.tuple_windows().filter(|(a, b)| a < b).count() as u16
 }
 
 pub fn part2(input: &String) -> u16 {
-    let report = input.lines().map(|s: &str| s.parse::<u16>().unwrap());
-    report.clone().zip(report.skip(3)).filter(|(a, b)| a < b).count() as u16
+    let report = input.lines().map(|s| s.parse::<u16>().unwrap());
+    report.tuple_windows().filter(|(a, _, _, d)| a < d).count() as u16
 }
