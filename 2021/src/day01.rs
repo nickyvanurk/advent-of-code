2c8360c17febbd03019323d83372f758713caa31
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-01 11:38:45 +0100

    Refactor day01

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,48 +1,9 @@
 pub fn part1(input: &String) -> u32 {
-    let report: Vec<u32> = input
-        .trim()
-        .split('\n')
-        .map(|s: &str| s.parse::<u32>().unwrap())
-        .collect();
-    
-    let mut previous_depth = report.first().unwrap();
-    let mut depth_increased_count = 0;
-    for depth in report.iter().skip(1) {
-        if depth > previous_depth {
-            depth_increased_count += 1;
-        }
-
-        previous_depth = depth;
-    }
-
-    depth_increased_count
+    let report = input.lines().map(|s: &str| s.parse::<u32>().unwrap());
+    report.clone().zip(report.skip(1)).filter(|(a, b)| a < b).count() as u32
 }
 
 pub fn part2(input: &String) -> u32 {
-    let report: Vec<u32> = input
-        .trim()
-        .split('\n')
-        .map(|s: &str| s.parse::<u32>().unwrap())
-        .collect();
-
-    let mut previous_depth: u32 = report.iter().take(3).sum();
-    let mut depth_increased_count = 0;
-    for (i, _) in report.iter().skip(1).enumerate() {
-        if report.len() - i < 3 {
-            break;
-        }
-
-        let a = report.iter().nth(i).unwrap();
-        let b = report.iter().nth(i + 1).unwrap();
-        let c = report.iter().nth(i + 2).unwrap();
-        let depth = a + b + c;
-
-        if depth > previous_depth {
-            depth_increased_count += 1;
-        }
-
-        previous_depth = depth
-    }
-    
-    depth_increased_count
+    let report = input.lines().map(|s: &str| s.parse::<u32>().unwrap());
+    report.clone().zip(report.skip(3)).filter(|(a, b)| a < b).count() as u32
 }
