ad06ca8a0c34e73b6d4aa6622e1a563daa87dd16
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-01 11:55:52 +0100

    Change u32 to u16

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,9 +1,9 @@
-pub fn part1(input: &String) -> u32 {
-    let report = input.lines().map(|s: &str| s.parse::<u32>().unwrap());
-    report.clone().zip(report.skip(1)).filter(|(a, b)| a < b).count() as u32
+pub fn part1(input: &String) -> u16 {
+    let report = input.lines().map(|s: &str| s.parse::<u16>().unwrap());
+    report.clone().zip(report.skip(1)).filter(|(a, b)| a < b).count() as u16
 }
 
-pub fn part2(input: &String) -> u32 {
-    let report = input.lines().map(|s: &str| s.parse::<u32>().unwrap());
-    report.clone().zip(report.skip(3)).filter(|(a, b)| a < b).count() as u32
+pub fn part2(input: &String) -> u16 {
+    let report = input.lines().map(|s: &str| s.parse::<u16>().unwrap());
+    report.clone().zip(report.skip(3)).filter(|(a, b)| a < b).count() as u16
 }
