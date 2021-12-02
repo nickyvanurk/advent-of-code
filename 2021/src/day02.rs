4ee00612751a108ac39440bb657b6e7d2b2557a1
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-02 12:45:49 +0100

    Refactor day02

--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,20 +1,6 @@
-struct Submarine {
-    position: u32,
-    depth: u32,
-    aim: u32,
-}
-
 pub fn part1(input: &String) -> u32 {
-    let input: Vec<(&str, u32)> = input.trim().split('\n').map(|s| {
-        let data: Vec<&str> = s.split(' ').collect();
-        (data[0], data[1].parse::<u32>().unwrap())
-    }).collect();
-
-    let mut submarine = Submarine {
-        position: 0,
-        depth: 0,
-        aim: 0,
-    };
+    let input = parse_input(input);
+    let mut submarine = build_submarine();
     
     for (instruction, value) in input {
         match instruction {
@@ -29,16 +15,8 @@ pub fn part1(input: &String) -> u32 {
 }
 
 pub fn part2(input: &String) -> u32 {
-    let input: Vec<(&str, u32)> = input.trim().split('\n').map(|s| {
-        let data: Vec<&str> = s.split(' ').collect();
-        (data[0], data[1].parse::<u32>().unwrap())
-    }).collect();
-
-    let mut submarine = Submarine {
-        position: 0,
-        depth: 0,
-        aim: 0,
-    };
+    let input = parse_input(input);
+    let mut submarine = build_submarine();
     
     for (instruction, value) in input {
         match instruction {
@@ -53,4 +31,25 @@ pub fn part2(input: &String) -> u32 {
     }
 
     submarine.position * submarine.depth
+}
+
+struct Submarine {
+    position: u32,
+    depth: u32,
+    aim: u32,
+}
+
+fn parse_input(input: &String) -> Vec<(&str, u32)> {
+    input.trim().split('\n').map(|s| {
+        let data: Vec<&str> = s.split(' ').collect();
+        (data[0], data[1].parse::<u32>().unwrap())
+    }).collect()
+}
+
+fn build_submarine() -> Submarine {
+    Submarine {
+        position: 0,
+        depth: 0,
+        aim: 0,
+    }
 }
