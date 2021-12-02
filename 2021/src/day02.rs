71197ffd4546be6df614f4db8a1be77a6cda528f
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-02 12:42:19 +0100

    Add day02 part2

--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,6 +1,7 @@
 struct Submarine {
     position: u32,
-    depth: u32
+    depth: u32,
+    aim: u32,
 }
 
 pub fn part1(input: &String) -> u32 {
@@ -12,6 +13,7 @@ pub fn part1(input: &String) -> u32 {
     let mut submarine = Submarine {
         position: 0,
         depth: 0,
+        aim: 0,
     };
     
     for (instruction, value) in input {
@@ -27,10 +29,28 @@ pub fn part1(input: &String) -> u32 {
 }
 
 pub fn part2(input: &String) -> u32 {
-    let _input: Vec<(&str, u32)> = input.trim().split('\n').map(|s| {
+    let input: Vec<(&str, u32)> = input.trim().split('\n').map(|s| {
         let data: Vec<&str> = s.split(' ').collect();
         (data[0], data[1].parse::<u32>().unwrap())
     }).collect();
 
-    0
+    let mut submarine = Submarine {
+        position: 0,
+        depth: 0,
+        aim: 0,
+    };
+    
+    for (instruction, value) in input {
+        match instruction {
+            "forward" => {
+                submarine.position += value;
+                submarine.depth += submarine.aim * value;
+            },
+            "up" => submarine.aim -= value,
+            "down" => submarine.aim += value,
+            _ => println!("Invalid instruction")
+        }
+    }
+
+    submarine.position * submarine.depth
 }
