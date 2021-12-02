5be17186df9f8652b96cbe1b2657438d0670ef9f
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-02 12:36:48 +0100

    Add day02 part1

--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,19 +1,36 @@
+struct Submarine {
+    position: u32,
+    depth: u32
+}
+
 pub fn part1(input: &String) -> u32 {
-    let _input: Vec<u32> = input
-        .trim()
-        .split('\n')
-        .map(|s: &str| s.parse::<u32>().unwrap())
-        .collect();
+    let input: Vec<(&str, u32)> = input.trim().split('\n').map(|s| {
+        let data: Vec<&str> = s.split(' ').collect();
+        (data[0], data[1].parse::<u32>().unwrap())
+    }).collect();
+
+    let mut submarine = Submarine {
+        position: 0,
+        depth: 0,
+    };
     
-    0
+    for (instruction, value) in input {
+        match instruction {
+            "forward" => submarine.position += value,
+            "up" => submarine.depth -= value,
+            "down" => submarine.depth += value,
+            _ => println!("Invalid instruction")
+        }
+    }
+
+    submarine.position * submarine.depth
 }
 
 pub fn part2(input: &String) -> u32 {
-    let _input: Vec<u32> = input
-        .trim()
-        .split('\n')
-        .map(|s: &str| s.parse::<u32>().unwrap())
-        .collect();
+    let _input: Vec<(&str, u32)> = input.trim().split('\n').map(|s| {
+        let data: Vec<&str> = s.split(' ').collect();
+        (data[0], data[1].parse::<u32>().unwrap())
+    }).collect();
 
     0
 }
