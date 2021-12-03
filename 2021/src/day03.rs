129bc5f3800221b2bf35de5af9e20b2d69ad08ca
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-03 11:00:09 +0100

    Refactor solution

--- 2021/src/day03.rs
+++ 2021/src/day03.rs
@@ -1,17 +1,10 @@
 pub fn part1(input: &String) -> u32 {
-    let input = input.trim().lines().map(|s| u16::from_str_radix(s, 2).unwrap());
-    let mut nth_bit_num = [0; 12];
-    let mut input_length = 0;
+    let input: Vec<u16> = input.trim().lines().map(|s| u16::from_str_radix(s, 2).unwrap()).collect();
 
-    for data in input {
-        input_length += 1;
-
-        for i in 0..12 {
-            nth_bit_num[i] += data >> (11 - i) & 1;
-        }
+    let mut bits: String = String::from("");
+    for i in 0..12 {
+        bits += &get_most_common_bit(&input, i).to_string();
     }
-    
-    let bits: String = nth_bit_num.map(|i| if i > input_length / 2 { '1' } else { '0' }).iter().collect();
     
     let gamma_rate = u16::from_str_radix(&bits, 2).unwrap();
     let epsilon_rate = !gamma_rate & 0b0000111111111111;
@@ -25,21 +18,11 @@ pub fn part2(input: &String) -> u32 {
     let mut oxygen_generator_rating = 0;
     let mut input_left: Vec<u16> = input.clone();
     for i in 0..12 {
-        let mut bit_num = 0;
-        let mut input_length = 0;
-
-        for data in input_left.iter() {
-            input_length += 1;
-            bit_num += data >> (11 - i) & 1;
-        }
-
-        let bit = if bit_num >= (input_length - bit_num) { 1 } else { 0 };
+        let bit = get_most_common_bit(&input_left, i);
 
         input_left = input_left.iter().cloned().filter(|data| {
             bit == data >> (11 - i) & 1
         }).collect();
-
-   
 
         if input_left.iter().count() == 1 {
             oxygen_generator_rating = *input_left.first().unwrap();
@@ -48,17 +31,9 @@ pub fn part2(input: &String) -> u32 {
     }
 
     let mut co2_scrubber_rating = 0;
-    input_left = input.clone();
+    let mut input_left = input.clone();
     for i in 0..12 {
-        let mut bit_num = 0;
-        let mut input_length = 0;
-
-        for data in input_left.iter() {
-            input_length += 1;
-            bit_num += data >> (11 - i) & 1;
-        }
-
-        let bit = if bit_num >= (input_length - bit_num) { 1 } else { 0 };
+        let bit = get_most_common_bit(&input_left, i);
 
         input_left = input_left.iter().cloned().filter(|data| {
             bit != data >> (11 - i) & 1
@@ -71,4 +46,16 @@ pub fn part2(input: &String) -> u32 {
     }
     
     oxygen_generator_rating as u32 * co2_scrubber_rating as u32
+}
+
+fn get_most_common_bit(input: &Vec<u16>, idx: usize) -> u16 {
+    let mut bit_num = 0;
+    let mut input_length = 0;
+
+    for data in input.iter() {
+        input_length += 1;
+        bit_num += data >> (11 - idx) & 1;
+    }
+
+    if bit_num >= (input_length - bit_num) { 1 } else { 0 }
 }
