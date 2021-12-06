86e79ee2e79ded9b720b574a188bb8a25e7cc8f4
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-06 11:37:43 +0100

    Add day 06 part 1

--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -1,11 +1,23 @@
 pub fn part1(input: &String) -> u32 {
-    let input: Vec<u8> = input.trim().split(',').map(|v| v.parse().unwrap()).collect();
+    let mut input: Vec<u8> = input.trim().split(',').map(|v| v.parse::<u8>().unwrap()).collect();
+    let days = 80;
 
-    0
+    for _ in 1..=days {
+        for i in 0..input.len() {
+            if input[i] >= 1 {
+                input[i] -= 1
+            } else {
+                input[i] = 6;
+                input.push(8);
+            }
+        }
+    }
+
+    input.len() as u32
 }
 
 pub fn part2(input: &String) -> u32 {
     let input: Vec<u8> = input.trim().split(',').map(|v| v.parse().unwrap()).collect();
-    
+
     0
 }
