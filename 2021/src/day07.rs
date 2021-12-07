6d2b56393367c359a92311ec61e4996d65d35c1e
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-07 10:07:15 +0100

    Add day 07 part 1

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -1,9 +1,20 @@
-use itertools::Itertools;
-
 pub fn part1(input: &String) -> u32 {
     let input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
+    let mut least_fuel: u32 = u32::MAX;
 
-    0
+    for &value in &input {
+        let mut fuel = 0;
+
+        for &x_position in &input {
+            fuel += i32::abs(x_position as i32 - value as i32) as u32
+        }
+
+        if fuel < least_fuel {
+            least_fuel = fuel;
+        }
+    }
+
+    least_fuel
 }
 
 pub fn part2(input: &String) -> u32 {
