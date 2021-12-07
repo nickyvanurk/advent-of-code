ab834b283dbbdecffffc8661df2868d473994f90
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-07 11:19:58 +0100

    Generalize part 2 to work with all input

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -15,16 +15,21 @@ pub fn part1(input: &String) -> u32 {
 pub fn part2(input: &String) -> u32 {
     let input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
 
-    let mean = input.iter().sum::<u32>() / input.len() as u32;
-    let mut fuel = 0;
+    let mean = input.iter().sum::<u32>() as f32 / input.len() as f32;
+    let mut least_fuel = u32::MAX;
 
-    for &x_position in &input {
-        let n = i32::abs(x_position as i32 - mean as i32) as u32;
-    
-        for i in 1..=n {
-            fuel += i;
+    for value in [mean.floor(), mean.ceil()] {
+        let mut fuel = 0;
+
+        for &x_position in &input {
+            let n = i32::abs(x_position as i32 - value as i32) as u32;
+            fuel += n * (n + 1) / 2;
+        }
+
+        if fuel < least_fuel {
+            least_fuel = fuel;
         }
     }
 
-    fuel
+    least_fuel
 }
