62263c42edfa95ad07f28742537820c2e555202f
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-07 10:32:02 +0100

    Speed up part 2

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -19,21 +19,15 @@ pub fn part1(input: &String) -> u32 {
 
 pub fn part2(input: &String) -> u32 {
     let input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
-    let mut least_fuel: u32 = u32::MAX;
 
-    for value in 0..=*input.iter().max().unwrap() {
-        let mut fuel = 0;
+    let mean = input.iter().sum::<u32>() / input.len() as u32;
+    let mut least_fuel = 0;
 
-        for &x_position in &input {
-            let n = i32::abs(x_position as i32 - value as i32) as u32;
-        
-            for i in 1..=n {
-                fuel += i;
-            }
-        }
-
-        if fuel < least_fuel {
-            least_fuel = fuel;
+    for &x_position in &input {
+        let n = i32::abs(x_position as i32 - mean as i32) as u32;
+    
+        for i in 1..=n {
+            least_fuel += i;
         }
     }
 
