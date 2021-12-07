2ea958512be82adcb7dd1744d722342ec66bfe5f
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-07 10:26:33 +0100

    Add day 07 part 2

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -19,6 +19,23 @@ pub fn part1(input: &String) -> u32 {
 
 pub fn part2(input: &String) -> u32 {
     let input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
+    let mut least_fuel: u32 = u32::MAX;
 
-    0
+    for value in 0..=*input.iter().max().unwrap() {
+        let mut fuel = 0;
+
+        for &x_position in &input {
+            let n = i32::abs(x_position as i32 - value as i32) as u32;
+        
+            for i in 1..=n {
+                fuel += i;
+            }
+        }
+
+        if fuel < least_fuel {
+            least_fuel = fuel;
+        }
+    }
+
+    least_fuel
 }
