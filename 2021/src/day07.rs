d2cf428c48eaca8c1924cd7c7f8343a2db370bff
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-07 10:43:23 +0100

    Speed up part 1

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -1,35 +1,30 @@
 pub fn part1(input: &String) -> u32 {
-    let input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
-    let mut least_fuel: u32 = u32::MAX;
+    let mut input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
 
-    for &value in &input {
-        let mut fuel = 0;
+    input.sort();
+    let median = input[input.len() / 2];
+    let mut fuel = 0;
 
-        for &x_position in &input {
-            fuel += i32::abs(x_position as i32 - value as i32) as u32
-        }
-
-        if fuel < least_fuel {
-            least_fuel = fuel;
-        }
+    for &x_position in &input {
+        fuel += i32::abs(x_position as i32 - median as i32) as u32
     }
 
-    least_fuel
+    fuel
 }
 
 pub fn part2(input: &String) -> u32 {
     let input: Vec<u32>= input.trim().split(',').map(|v| v.parse::<u32>().unwrap()).collect();
 
     let mean = input.iter().sum::<u32>() / input.len() as u32;
-    let mut least_fuel = 0;
+    let mut fuel = 0;
 
     for &x_position in &input {
         let n = i32::abs(x_position as i32 - mean as i32) as u32;
     
         for i in 1..=n {
-            least_fuel += i;
+            fuel += i;
         }
     }
 
-    least_fuel
+    fuel
 }
