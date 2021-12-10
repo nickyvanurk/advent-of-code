eb6902ce2523a183f615ae7fef23c2db53cc7c0d
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-10 10:51:53 +0100

    Add day 10 part 2

--- 2021/src/day10.rs
+++ 2021/src/day10.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 
 pub fn part1(input: &String) -> u32 {
     let pairs = HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]);
-    let mut opening_chars: Vec<char> = vec![];
-
     let score_table = HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
     let mut score = 0;
 
     input.lines().for_each(|l| {
+        let mut opening_chars: Vec<char> = vec![];
+
         l.chars().for_each(|c| {
             if pairs.contains_key(&c) {
                 opening_chars.push(c);
@@ -20,8 +20,29 @@ pub fn part1(input: &String) -> u32 {
     score
 }
 
-pub fn part2(input: &String) -> u16 {
-    let input = input.lines();
+pub fn part2(input: &String) -> u64 {
+    let pairs = HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]);
+    let score_table = HashMap::from([(')', 1), (']', 2), ('}', 3), ('>', 4)]);
+    let mut scores = vec![];
 
-    0
+    input.lines().for_each(|l| {
+        let mut opening_chars: Vec<char> = vec![];
+
+        for c in l.chars() {
+            if pairs.contains_key(&c) {
+                opening_chars.push(c);
+            } else if pairs[&opening_chars.pop().unwrap()] != c {
+                return;
+            }
+        }
+
+        let mut score = 0;
+        for c in opening_chars.iter().rev() {
+            score = score * 5 + score_table[&pairs[&c]];
+        }
+        scores.push(score);
+    });
+
+    scores.sort();
+    scores[scores.len() / 2]
 }
