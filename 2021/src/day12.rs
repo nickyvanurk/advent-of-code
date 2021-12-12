db88d3b605c70d4ad42582e89b56f67fceaec330
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-12 20:53:07 +0100

    Add day 12 part 2

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 
 pub fn part1(input: &String) -> u32 {
@@ -12,11 +13,16 @@ pub fn part1(input: &String) -> u32 {
     get_paths("start", &mut vec![], &cavern)
 }
 
-pub fn part2(input: &String) -> u16 {
+pub fn part2(input: &String) -> u32 {
     let input: Vec<Vec<&str>> = input.lines().map(|s| s.split('-').collect()).collect();
+    let mut cavern: HashMap::<&str, Vec<&str>> = HashMap::new();
+    
+    for entry in &input {
+        (*cavern.entry(entry[0]).or_insert(vec![])).push(entry[1]);
+        (*cavern.entry(entry[1]).or_insert(vec![])).push(entry[0]);
+    }
 
-
-    0
+    get_paths2("start", &mut vec![], &cavern)
 }
 
 fn get_paths<'a>(start: &'a str, visited: &mut Vec<&'a str>, cavern: &HashMap::<&str, Vec<&'a str>>) -> u32 {
@@ -34,6 +40,35 @@ fn get_paths<'a>(start: &'a str, visited: &mut Vec<&'a str>, cavern: &HashMap::<
         }
 
         paths += get_paths(cave, visited, cavern);
+        visited.pop();
+    }
+
+    paths
+}
+
+fn get_paths2<'a>(start: &'a str, visited: &mut Vec<&'a str>, cavern: &HashMap::<&str, Vec<&'a str>>) -> u32 {
+    let mut paths = 0;
+
+    visited.push(start);
+
+    if start == "end" {
+        return 1;
+    }
+
+    for &cave in &cavern[start] {
+        if cave == "start" {
+            continue;
+        }
+
+        let small_caves = visited.iter().map(|c| *c).filter(|&c| c.chars().all(char::is_lowercase)).collect::<Vec<&str>>();
+        let duplicate = small_caves.len() != small_caves.iter().unique().count();
+        let is_small_cave = cave.chars().all(char::is_lowercase);
+        
+        if duplicate && is_small_cave && visited.contains(&cave) {
+            continue;
+        }
+
+        paths += get_paths2(cave, visited, cavern);
         visited.pop();
     }
 
