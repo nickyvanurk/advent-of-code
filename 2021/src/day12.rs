e05eda8ec9544db8a50ba9ed31e2675ffaa7580a
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-12 19:55:53 +0100

    Add day 12 part 1

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -1,7 +1,15 @@
-pub fn part1(input: &String) -> u16 {
-    let input: Vec<Vec<&str>> = input.lines().map(|s| s.split('-').collect()).collect();
+use std::collections::HashMap;
 
-    0
+pub fn part1(input: &String) -> u32 {
+    let input: Vec<Vec<&str>> = input.lines().map(|s| s.split('-').collect()).collect();
+    let mut cavern: HashMap::<&str, Vec<&str>> = HashMap::new();
+    
+    for entry in &input {
+        (*cavern.entry(entry[0]).or_insert(vec![])).push(entry[1]);
+        (*cavern.entry(entry[1]).or_insert(vec![])).push(entry[0]);
+    }
+
+    get_paths("start", &mut vec![], &cavern)
 }
 
 pub fn part2(input: &String) -> u16 {
@@ -9,4 +17,25 @@ pub fn part2(input: &String) -> u16 {
 
 
     0
+}
+
+fn get_paths<'a>(start: &'a str, visited: &mut Vec<&'a str>, cavern: &HashMap::<&str, Vec<&'a str>>) -> u32 {
+    let mut paths = 0;
+
+    visited.push(start);
+
+    if start == "end" {
+        return 1;
+    }
+
+    for &cave in &cavern[start] {
+        if cave == "start" || (cave.chars().all(char::is_lowercase) && visited.contains(&cave)) {
+            continue;
+        }
+
+        paths += get_paths(cave, visited, cavern);
+        visited.pop();
+    }
+
+    paths
 }
