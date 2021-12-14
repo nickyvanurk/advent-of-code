5f1f6ac2463c44ce23ae78618dfdb8a96a07957d
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-14 14:39:34 +0100

    Add day 14 part 1

--- 2021/src/day14.rs
+++ 2021/src/day14.rs
@@ -1,7 +1,35 @@
-pub fn part1(input: &String) -> u32 {
-    let input = input.lines();
+use itertools::Itertools;
 
-    0
+pub fn part1(input: &String) -> u32 {
+    let mut template = String::from(input.lines().nth(0).unwrap());
+    let rules: Vec<Vec<&str>> = input.lines().skip(2).map(|l| l.split(" -> ").collect()).collect();
+    
+    for _step in 0..10 {
+        let mut last_char = template.chars().nth(0).unwrap();
+        let chars: Vec<char> = template.clone().chars().collect();
+        let mut insert_count = 0;
+
+        for i in 1..chars.len() {
+            let rule_key = format!("{}{}", last_char, chars[i]);
+
+            for rule in &rules {
+                if rule[0] == rule_key {
+                    template.insert_str(i + insert_count, rule[1]);
+                    insert_count += 1;
+                }
+            }
+
+            last_char = chars[i];
+        }
+    }
+
+    let quantities = template.chars()
+                             .sorted()
+                             .group_by(|&x| x)
+                             .into_iter().map(|(_, group)| group.count() as u32)
+                             .collect::<Vec<u32>>();
+
+    quantities.iter().max().unwrap() - quantities.iter().min().unwrap()
 }
 
 pub fn part2(input: &String) -> u32 {
