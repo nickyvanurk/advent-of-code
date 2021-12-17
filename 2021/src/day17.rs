9770fa8f113ec063298e416cd0e452c9c4a4d3a0
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-17 16:06:50 +0100

    Add day 17 part 2

--- 2021/src/day17.rs
+++ 2021/src/day17.rs
@@ -1,3 +1,5 @@
+// Note: I reversed the y order, negative is up
+
 pub fn part1(input: &String) -> u16 {
     let input = input.trim().replace("target area: ", "").replace("x=", "").replace("y=", "").replace(",", "").replace("-", "")
                            .split(' ').map(|range| range.split("..").map(|s| s.parse::<i16>().unwrap()).collect::<Vec<i16>>())
@@ -6,37 +8,21 @@ pub fn part1(input: &String) -> u16 {
     let target_area = TargetArea::new(&input);
     let mut probe = Probe::new();
    
-    let velocity = get_highest_arc_velocity(&mut probe, &target_area);
-    probe.set_velocity((velocity.x, velocity.y));
-
-    let mut highest_y_position= 0;
-    while !target_area.in_bounds(&probe) {
-        probe.update();
-
-        // I reversed the y order, negative is up
-        if probe.position.y < highest_y_position {
-            highest_y_position = probe.position.y;
-        }
-    }
-
-    i16::unsigned_abs(highest_y_position)
-}
-
-fn get_highest_arc_velocity(probe: &mut Probe, target_area: &TargetArea) -> Velocity {
-    // let max_x_velocity = get_max_x_velocity(probe, target_area);
-
     for x_velocity in 1..200 {
         for y_velocity in (1..200).rev() {
-            probe.reset();
-            probe.set_velocity((x_velocity, -y_velocity));
+            let mut highest_y_position= 0;
+
+            probe.reset_with_velocity((x_velocity, -y_velocity));
 
             while !target_area.is_overshot(&probe) {
-
                 probe.update();
 
+                if probe.position.y < highest_y_position {
+                    highest_y_position = probe.position.y;
+                }
+
                 if target_area.in_bounds(&probe) {
-                    probe.reset();
-                    return Velocity { x: x_velocity, y: -y_velocity };
+                    return i16::unsigned_abs(highest_y_position);
                 }
             }
         }
@@ -45,28 +31,32 @@ fn get_highest_arc_velocity(probe: &mut Probe, target_area: &TargetArea) -> Velo
     panic!("Should never be reached");
 }
 
-// fn get_max_x_velocity(probe: &mut Probe, target_area: &TargetArea) -> i16 {
-//     for x_velocity in (1..200).rev() {
-//         probe.reset();
-//         probe.set_velocity((x_velocity, 0));
+pub fn part2(input: &String) -> u16 {
+    let input = input.trim().replace("target area: ", "").replace("x=", "").replace("y=", "").replace(",", "").replace("-", "")
+                           .split(' ').map(|range| range.split("..").map(|s| s.parse::<i16>().unwrap()).collect::<Vec<i16>>())
+                           .collect::<Vec<Vec<i16>>>();
 
-//         while !target_area.is_overshot(&probe) {
-//             probe.update();
+    let target_area = TargetArea::new(&input);
+    let mut probe = Probe::new();
+   
+    let mut total_valid_velocities = 0;
 
-//             if target_area.in_bounds(&probe) {
-//                 probe.reset();
-//                 return x_velocity;
-//             }
-//         }
-//     }
+    for x_velocity in 1..200 {
+        for y_velocity in (-200..200).rev() {
+            probe.reset_with_velocity((x_velocity, -y_velocity));
 
-//     panic!("Should never be reached");
-// }
+            while !target_area.is_overshot(&probe) {
+                probe.update();
 
-pub fn part2(input: &String) -> u32 {
-    let input = input.lines();
-
-    0
+                if target_area.in_bounds(&probe) {
+                    total_valid_velocities += 1;
+                    break;
+                }
+            }
+        }
+    }
+    
+    total_valid_velocities
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -95,14 +85,9 @@ impl Probe {
         }
     }
 
-    fn reset(&mut self) {
+    fn reset_with_velocity(&mut self, velocity :(i16, i16)) {
         self.position.x = 0;
         self.position.y = 0;
-        self.velocity.x = 0;
-        self.velocity.y = 0;
-    }
-
-    fn set_velocity(&mut self, velocity :(i16, i16)) {
         self.velocity.x = velocity.0;
         self.velocity.y = velocity.1;
     }
@@ -143,18 +128,9 @@ impl TargetArea {
     }
 
     fn in_bounds(&self, probe: &Probe) -> bool {
-        self.in_bounds_x(probe) && self.in_bounds_y(probe)
-    }
-
-    fn in_bounds_x(&self, probe: &Probe) -> bool {
         let o = self.position;
         let p = probe.position;
-        o.x <= p.x && p.x <= o.x + self.width as i16
-    }
-
-    fn in_bounds_y(&self, probe: &Probe) -> bool {
-        let o = self.position;
-        let p = probe.position;
+        o.x <= p.x && p.x <= o.x + self.width as i16 &&
         o.y <= p.y && p.y <= o.y + self.height as i16
     }
 }
