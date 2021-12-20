5bf0ae36d40a9458c0c75e4673cd267ef4b873bb
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-20 17:44:23 +0100

    Parse input

--- 2021/src/day20.rs
+++ 2021/src/day20.rs
@@ -1,5 +1,9 @@
 pub fn part1(input: &String) -> u32 {
-    let input = input.lines();
+    let mut input = input.lines();
+    let algorithm = input.nth(0).unwrap().chars().collect::<Vec<char>>();
+    let mut image: Vec<Vec<char>> = input.skip(1).map(|l| l.chars().collect()).collect();
+
+    print_image(&image);
 
     0
 }
@@ -8,4 +12,13 @@ pub fn part2(input: &String) -> u32 {
     let input = input.lines();
 
     0
+}
+
+fn print_image(data: &Vec<Vec<char>>) {
+    for row in data {
+        for pixel in row {
+            print!("{}", pixel);
+        }
+        println!("");
+    }
 }
