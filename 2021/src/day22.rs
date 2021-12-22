42d864a894817963ebe55e3338fa266f6cbc1adc
Nicky van Urk <nickyvanurk@gmx.com>
2021-12-22 13:25:41 +0100

    Merge input ranges per axis

--- 2021/src/day22.rs
+++ 2021/src/day22.rs
@@ -1,7 +1,7 @@
 // Reboot reactor
 
 // Reactor:
-// 3D grid of cubes (octree)
+// 3D grid of cubes
 
 // Each cube on and off, 1 and 0.
 // They are all off at start of reboot
@@ -15,24 +15,32 @@
 // Execute reboot steps
 // How many cubes are on in -50 50 region?
 
-
 pub fn part1(input: &String) -> u32 {
     let input = input.lines();
+
+    let mut xs = vec![];
+    let mut ys = vec![];
+    let mut zs = vec![];
 
     for line in input {
         let mut s = line.split(&[' ', ','][..]);
 
-        let state = if s.next().unwrap() == "on" { 1 } else { 0 };
+        let state = s.next().unwrap();
         let x = parse_step_axis_into_tuple(s.next().unwrap());
         let y = parse_step_axis_into_tuple(s.next().unwrap());
         let z = parse_step_axis_into_tuple(s.next().unwrap());
-        
-        println!("{:?}", state);
-        println!("{:?}", x);
-        println!("{:?}", y);
-        println!("{:?}", z);
+
+        if state == "off" {
+            xs.push(x);
+            ys.push(z);
+            zs.push(y);
+        }
     }
 
+    merge_ranges(&mut xs);
+    merge_ranges(&mut ys);
+    merge_ranges(&mut zs);
+    
     0
 }
 
@@ -45,4 +53,20 @@ pub fn part2(input: &String) -> u32 {
 fn parse_step_axis_into_tuple(data: &str) -> (i32, i32) {
     let mut parts = data[2..].split("..");
     (parts.next().unwrap().parse::<i32>().unwrap(), parts.next().unwrap().parse::<i32>().unwrap())
+}
+
+fn merge_ranges(ranges: &mut Vec<(i32, i32)>) {
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+    let mut stack = vec![ranges[0]];
+
+    for range in ranges.iter() {
+        let last = stack.last_mut().unwrap();
+        if range.0 > last.1 {
+            stack.push(*range);
+        } else if range.1 > last.1 {
+            last.1 = range.1;
+        }
+    }
+
+    *ranges = stack;
 }
