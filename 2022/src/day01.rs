6d3c8ac9fc0d993e7212dbbedb110d59ed569b3a
Nicky van Urk <nickyvanurk@gmx.com>
2022-12-01 09:44:56 +0100

    Update input to be always trimmed

--- 2022/src/day01.rs
+++ 2022/src/day01.rs
@@ -1,13 +1,13 @@
 use itertools::Itertools;
 
-pub fn part1(input: &String) -> u32 {
-    input.trim().split("\n\n").map(|s| {
+pub fn part1(input: &str) -> u32 {
+    input.split("\n\n").map(|s| {
         s.split('\n').map(|n| n.parse::<u32>().unwrap()).sum()
     }).max().unwrap()
 }
 
-pub fn part2(input: &String) -> u32 {
-    input.trim().split("\n\n").map(|s| {
+pub fn part2(input: &str) -> u32 {
+    input.split("\n\n").map(|s| {
         s.split('\n').map(|n| n.parse::<u32>().unwrap()).sum::<u32>()
     }).sorted().rev().take(3).sum()
 }
