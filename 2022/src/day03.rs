f32ee193c1df0bff1e12d2fb4ad5105976c4c59f
Nicky van Urk <nickyvanurk@gmx.com>
2022-12-03 16:26:04 +0100

    Refactor to make more concise

--- 2022/src/day03.rs
+++ 2022/src/day03.rs
@@ -1,23 +1,18 @@
+use std::collections::HashSet;
+
 pub fn part1(input: &str) -> u32 {
     let input = input.lines().map(|s| (&s[..s.len()/2], &s[s.len()/2..]));
     let mut sum = 0;
 
     for (a, b) in input {
-        let mut freq1 = vec![0; 58];
-        for c in a.chars() {
-            freq1[c as usize - 'A' as usize] += 1;
-        }
+        let set: HashSet<char> = a.chars().collect();
+        let common_char = b.chars().filter(|c| set.contains(&c)).nth(0).unwrap();
 
-        let mut freq2 = vec![0; 58];
-        for c in b.chars() {
-            freq2[c as usize - 'A' as usize] += 1;
-        }
-
-        for i in 0..58 {
-            if std::cmp::min(freq1[i], freq2[i]) > 0 {
-                sum += if i > 25 { i - 6 - 25 } else { i + 27 } as u32;
-            }
-        }
+        sum += if common_char.is_lowercase() {
+            (common_char as u32 - 'a' as u32) + 1
+        } else {
+            (common_char as u32 - 'A' as u32) + 27
+        };
     }
 
     sum
@@ -28,30 +23,15 @@ pub fn part2(input: &str) -> u32 {
     let mut sum = 0;
 
     for i in (0..input.len()).step_by(3) {
-        let a = input[i];
-        let b = input[i + 1];
-        let c = input[i + 2];
-
-        let mut freq1 = vec![0; 58];
-        for c in a.chars() {
-            freq1[c as usize - 'A' as usize] += 1;
-        }
-
-        let mut freq2 = vec![0; 58];
-        for c in b.chars() {
-            freq2[c as usize - 'A' as usize] += 1;
-        }
-
-        let mut freq3 = vec![0; 58];
-        for c in c.chars() {
-            freq3[c as usize - 'A' as usize] += 1;
-        }
-
-        for i in 0..58 {
-            if std::cmp::min(freq1[i], std::cmp::min(freq2[i], freq3[i])) > 0 {
-                sum += if i > 25 { i - 6 - 25 } else { i + 27 } as u32;
-            }
-        }
+        let set1: HashSet<char> = input[i].chars().collect();
+        let set2: HashSet<char> = input[i + 1].chars().collect();
+        let common_char = input[i + 2].chars().filter(|c| set1.contains(&c) && set2.contains(&c)).nth(0).unwrap();
+        
+        sum += if common_char.is_lowercase() {
+            (common_char as u32 - 'a' as u32) + 1
+        } else {
+            (common_char as u32 - 'A' as u32) + 27
+        };
     }
 
     sum
