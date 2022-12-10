2389f69b502b509127c8aa7d7843f3c5334701ae
Nicky van Urk <nickyvanurk@gmx.com>
2022-12-10 16:50:55 +0100

    Add day 10 part 2

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -1,5 +1,3 @@
-
-
 pub fn part1(input: &str) -> i32 {
     let mut instructions: Vec<Vec<&str>> = input.lines().map(|s| s.split_whitespace().collect()).rev().collect();
     let mut register = 1;
@@ -36,6 +34,47 @@ pub fn part1(input: &str) -> i32 {
     signal_strength
 }
 
-pub fn part2(input: &str) -> u32 {
-    0
+pub fn part2(input: &str) {
+    let mut instructions: Vec<Vec<&str>> = input.lines().map(|s| s.split_whitespace().collect()).rev().collect();
+    let mut register = 1;
+    let mut cycles = 0;
+    let mut pixels: Vec<char> = "###.....................................".chars().collect();
+
+    let mut temp = 0;
+    loop {
+        cycles += 1;
+
+        print!("{}", if pixels[(cycles - 1) as usize % 40] == '#' { '#' } else { '.' });
+        if vec![40, 80, 120, 160, 200, 240].contains(&cycles) {
+            println!();
+        }
+
+        register += temp;
+        let ready = temp == 0;
+        temp = 0;
+
+        if ready {
+            let instruction = instructions.pop().unwrap();
+            match instruction[0] {
+                "noop" => (),
+                "addx" => temp = instruction[1].parse::<i32>().unwrap(),
+                _ => (),
+            }
+        }
+
+        pixels = vec!['.'; 40];
+        if register >= -1 && register <= 40 {
+            for index in vec![register - 1, register, register + 1] {
+                if index >= 0 && index <= 39 {
+                    pixels[index as usize] = '#';
+                }
+            }
+        }
+
+        if instructions.len() == 0 && temp == 0 {
+            break;
+        }
+    }
+    
+    println!();
 }
